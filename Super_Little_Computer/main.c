2475be07231efd532474b90f363239abef4a82d0
ila004 <166114805+ila004@users.noreply.github.com>
2025-11-16 21:34:58 +0100

    Update main.c

--- Super_Little_Computer/main.c
+++ Super_Little_Computer/main.c
@@ -4,16 +4,20 @@
 int main(void) {
 	size_t l_len = 0;
 	char* l = NULL;
-	long res = 0;
+	long long res = 0;
 	while (getline(&l, &l_len, stdin) != -1) {
+		int error = 0;
 		int sign = 0, count = 0;
 		char* str = l;
-		res = operation_lowPriority(&str);
+		res = operation_lowPriority(&str, &error);
+		if(error != 0){
+			continue;
+		}
 		if (res < 0) {
 			sign = 1;
 			res = -res;
 		}
-		long n = res;
+		long long n = res;
 		while (n > 0) {
 			n = n / 10;
 			count++;
@@ -22,7 +26,7 @@ int main(void) {
 		char* s = malloc(sizeof(char) * count);
 		if (!s) {
 			fwrite("Error\n", 1, 6, stderr);
-			exit(2);
+			continue;
 		}
 		s[count - 1] = '\0';
 		for (int i = count - 2; i >= 0; i--) {
