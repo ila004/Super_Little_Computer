554c08cfe1ee6e122bc3f4465ca0118b289be1ad
ila004 <166114805+ila004@users.noreply.github.com>
2025-11-16 20:58:00 +0100

    Update calc.h

--- Super_Little_Computer/calc.h
+++ Super_Little_Computer/calc.h
@@ -1,5 +1,8 @@
 #if !defined CALC_H
 #define CALC_H
-long operation_lowPriority(char** s);
+long long operation_lowPriority(char** s, int* error);
+long long parenthesis_highPriority(char** s, int* error);
+long long operation_highPriority(char** s, int* error);
 void skip_spaces(char**s);
+
 #endif //CALC_H
